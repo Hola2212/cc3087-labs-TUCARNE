831fa115aa9cdaa363a9bc60e15dcd280b7f10de
Hola2212 <juanpaflores06@gmail.com>
2026-08-16 21:20:21 -0600

    Added life cicle in Logcat

--- app/src/main/java/com/example/lab05composefeed/MainActivity.kt
+++ app/src/main/java/com/example/lab05composefeed/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.example.lab05composefeed
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab05composefeed.ui.screens.FeedScreen
 import com.example.lab05composefeed.ui.theme.Lab05ComposeFeedTheme
 
+private const val TAG = "LAB6_25454"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         enableEdgeToEdge()
         setContent {
             Lab05ComposeFeedTheme {
@@ -27,5 +27,30 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
